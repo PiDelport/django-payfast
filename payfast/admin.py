7a6ae520b36b0b9baea0921bd0a58f1bc4a1533f
Mike Korobov <kmike84@gmail.com>
2010-12-05 21:42:31 +0500

    Stray field is removed

--- payfast/admin.py
+++ payfast/admin.py
@@ -6,7 +6,7 @@ class PayFastOrderAdmin(admin.ModelAdmin):
     list_display = ['m_payment_id', 'pf_payment_id', 'user', 'created_at', 'amount_gross',
                     'payment_status', 'item_name']
     list_filter = ['trusted', 'payment_status']
-    search_fields = ['m_payment_id', 'pf_payment_id', 'item_name', 'Extra3',
+    search_fields = ['m_payment_id', 'pf_payment_id', 'item_name',
                      'user__username', 'name_first', 'name_last', 'email_address']
     raw_id_fields = ['user']
     date_hierarchy = 'created_at'
