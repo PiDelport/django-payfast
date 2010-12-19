29d7e1ada45ca0c1ae49a162dc4c6b94aa440a8d
Mike Korobov <kmike84@gmail.com>
2010-12-19 15:30:55 +0500

    Force the usage of test credentials in sandbox

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-payfast',
-    version='0.2.0',
+    version='0.2.1',
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
 
