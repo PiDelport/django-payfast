512ab3e68eb6a22cd1f300676ce64c49c4e0e351
Piet Delport <pjdelport@gmail.com>
2015-05-20 00:07:52 +0200

    Modernize the URLconf modules
    This makes the tests pass up to Django 1.7.
    
    --HG--
    branch : django-updates

--- payfast_tests/urls.py
+++ payfast_tests/urls.py
@@ -1,8 +1,25 @@
-from django.conf.urls.defaults import *
-from django.contrib import admin
-admin.autodiscover()
+import django
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^payfast/', include('payfast.urls')),
-)
+if django.VERSION < (1, 3):
+    # Work around https://code.djangoproject.com/ticket/15343
+    from django.conf.urls.defaults import handler404, handler500
+
+if django.VERSION < (1, 4):
+    from django.conf.urls.defaults import url, include
+else:
+    from django.conf.urls import url, include
+
+from django.contrib import admin
+
+import payfast.urls
+
+
+if django.VERSION < (1, 7):
+    admin.autodiscover()
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^payfast/', include(payfast.urls)),
+]
+
