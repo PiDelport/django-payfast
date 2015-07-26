2b9900b63d6eb6f27ecbe8461eb61a17240faaf0
Piet Delport <pjdelport@gmail.com>
2015-07-26 08:05:45 +0200

    (Bump version: 0.2.2 -> 0.3.dev)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-payfast',
-    version='0.2.2',
+    version='0.3.dev',
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
 
