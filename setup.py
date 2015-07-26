b2f019ea838bb26c4e6de9a5f68d28ba3fb464b2
Piet Delport <pjdelport@gmail.com>
2015-07-26 07:34:00 +0200

    Packaging: Switch from distutils to setuptools, and use find_packages()
    This includes the payfast.migrations package in the distribution too.
    
    payfast_tests is now explicitly excluded from installation (rather than
    implicitly as before).
    
    --HG--
    branch : django-updates

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-payfast',
@@ -7,7 +7,7 @@ setup(
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
 
-    packages=['payfast', 'payfast.south_migrations'],
+    packages=find_packages(exclude=['payfast_tests']),
 
     url='http://bitbucket.org/kmike/django-payfast/',
     download_url = 'http://bitbucket.org/kmike/django-payfast/get/tip.gz',
